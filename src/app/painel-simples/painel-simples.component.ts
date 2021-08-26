6ebd8c76f7b9797d7307c44cdee4544ac9db9df3
wander rodrigues <wanderrodrigues_@outlook.com>
2021-08-26 05:15:44 -0300

    :brain: first commit

---
+++ src/app/painel-simples/painel-simples.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-painel-simples',
+  templateUrl: './painel-simples.component.html',
+  styleUrls: ['./painel-simples.component.css']
+})
+export class PainelSimplesComponent  {
+
+  constructor() { }
+
+
+}
