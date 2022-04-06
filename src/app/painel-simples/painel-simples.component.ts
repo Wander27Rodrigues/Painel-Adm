4f0d3a45a083db681ab01c1c1045a1025776b34f
wander rodrigues <wanderrodrigues_@outlook.com>
2022-04-06 20:04:01 -0300

    painel

--- src/app/painel-simples/painel-simples.component.ts
+++ src/app/painel-simples/painel-simples.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-painel-simples',
   templateUrl: './painel-simples.component.html',
   styleUrls: ['./painel-simples.component.css']
 })
-export class PainelSimplesComponent  {
+export class PainelSimplesComponent {
+  @Input() titulo = "Titulo do Painel";
+  @Input() col = 6;
+  @Input() tipo = "info";
 
   constructor() { }
-
 
 }
